f51d73ae99aff24e4a2f96aa9df6bbd97cd30ea2
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2024-10-07 20:32:36 -0400

    Continue Work on Nextcloud Template

---
+++ Server/Nextcloud/nextcloud.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+{
+  environment.etc."nextcloud-admin-pass".text = "";
+  services.nextcloud = {
+    enable = true;
+    # configureRedis = true;
+    package = pkgs.nextcloud29;
+    hostName = "nextcloud";
+    # https = true;
+    config.adminpassFile = "/etc/nextcloud-admin-pass";
+    # autoUpdateApps.enable = true;
+    # extraAppsEnable = true;
+    # extraApps = with config.services.nextcloud.package.packages.apps; {
+        # List of apps we want to install and are already packaged in
+        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
+        # inherit calendar notes tasks deck;
+    # };
+  };
+
+}
