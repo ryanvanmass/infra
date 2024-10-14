7964eb271fd7a9bc07e0a85f204d60cd1d6b86b3
Ryan Van Massenhoven <65045018+ryanvanmass@users.noreply.github.com>
2024-10-14 08:14:47 -0400

    Begin Work on Fileshare.vanmassenhoven.com

---
+++ Server/Nextcloud/fileshare_vanmassenhoven_com.nix
@@ -0,0 +1,26 @@
+{ config, lib, pkgs, ... }:
+{
+  networking.firewall.allowedTCPPorts = [ 443 80 ];
+  services.nextcloud = {
+    enable = true;
+    # configureRedis = true;
+    package = pkgs.nextcloud29;
+    hostName = "nextcloud";
+    https = true;
+    config.adminpassFile = "/etc/nextcloud-admin-pass";
+    settings = {
+      overwriteProtocol = "https";
+      trusted_domains = [
+        ""
+      ];
+    };
+    # autoUpdateApps.enable = true;
+    # extraAppsEnable = true;
+    # extraApps = with config.services.nextcloud.package.packages.apps; {
+        # List of apps we want to install and are already packaged in
+        # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
+        # inherit calendar notes tasks deck;
+    # };
+  };
+
+}
